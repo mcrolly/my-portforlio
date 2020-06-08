5bf4e4b46ebf6ed06d5e5e34ce8b03639cf4516d
Chinedum McRolly Nwangwu <mcrolly@gmail.com>
2020-06-07 20:56:54 -0500

    Fix dist folder build issue for codeBuild

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports ={
   entry: './js/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'distr'),
     filename: 'bundle.js'
   },
   module: {
