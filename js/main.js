3327e93d7ac301b19fd169e88bc7a0d8d7c7a8f9
Chinedum McRolly Nwangwu <mcrolly@gmail.com>
2020-06-08 00:10:03 -0500

    start using react components

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log('loaded react-dom')
+const myWork = [
+  {
+  'title': "Work Example",
+  'image':{
+    'desc':"example screenshot of a project involving code",
+    'src':"images/example1.png",
+    'comment':""
+  }
+},
+{
+'title': "Portfolio Boilerplate",
+'image':{
+  'desc':"A serverless Portfolio",
+  'src':"images/example2.png",
+  'comment':`“Chemistry” by Surian Soosay is licensed under CC BY 2.0
+  https://www.flickr.com/photos/ssoosay/4097410999`
+}
+},
+{
+'title': "Work Example",
+'image':{
+  'desc':"example screenshot of a project involving cats",
+  'src':"images/example3.png",
+  'comment':`“Bengal cat” by roberto shabs is licensed under CC BY 2.0
+                 https://www.flickr.com/photos/37287295@N00/2540855181`
+}
+}
+
+]
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'))
